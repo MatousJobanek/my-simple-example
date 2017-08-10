0e4965466d66e9ceeb413a6956e2f1b6ece758b2
Matous Jobanek <mjobanek@redhat.com>
2017-08-10 12:11:08 +0200

    added tests

---
+++ src/test/java/my/simple/example/FirstSimpleTest.java
@@ -0,0 +1,11 @@
+package my.simple.example;
+
+import org.junit.Test;
+
+public class FirstSimpleTest {
+
+    @Test
+    public void runFirstSimpleTest(){
+
+    }
+}
---
+++ src/test/java/my/simple/example/SecondSimpleTest.java
@@ -0,0 +1,11 @@
+package my.simple.example;
+
+import org.junit.Test;
+
+public class SecondSimpleTest {
+
+    @Test
+    public void runSecondSimpleTest(){
+        
+    }
+}
